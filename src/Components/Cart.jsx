ebabf676fc3a1f69010d0a594876a793394199bd
dahabiya <130239358+dahabiya@users.noreply.github.com>
2024-01-24 12:22:14 +0530

    Update Cart  image

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -68,7 +68,7 @@ function Cart() {
         <div> 
 
          <div className="navv"><Link to='/'>
-                <img className='bewakoof-logo' src='/images/bewakoof.jpg' alt="Bewakoof-Logo"/></Link>
+                <img className='bewakoof-logo' src='https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg' alt="Bewakoof-Logo"/></Link>
                 </div>
           
           {
